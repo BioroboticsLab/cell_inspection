4b86b43127dbd2b88b60f6f41334173ffc012941
sophie <sophie.zabel@gmx.de>
2019-08-22 17:46:55 +0200

    save Training (as in train.py), if/else (as in test.py), small details changed

--- train_test.py
+++ train_test.py
@@ -16,7 +16,16 @@ import numpy as np
 import itertools
 
 
-def train_net(trainloader, classes, net, criterion, optimizer, epoch, epoches, running_loss, t_loss, train_i, train_loss, train_images, train_image_label, save_epoch):
+def store_json(train_loss, epoches):
+    d = {
+        "Training loss" : train_loss,
+        "Epoch" : epoches
+    }
+    f = open('../Training/trainloss+epoches.json', 'w')
+    json.dump(d, f, indent = 4)
+
+
+def train_net(trainloader, classes, b_size, net, criterion, optimizer, epoch, epoches, running_loss, t_loss, train_i, train_loss, train_images, train_image_label, save_epoch):
     for index, batch in enumerate(trainloader, 0):
         inputs, labels = batch
         # index = running counter
@@ -32,6 +41,7 @@ def train_net(trainloader, classes, net, criterion, optimizer, epoch, epoches, r
 
         # forward + backward + optimize
         outputs = net(inputs)
+
         loss = criterion(outputs, labels)
         loss.backward()
         optimizer.step()
@@ -46,15 +56,16 @@ def train_net(trainloader, classes, net, criterion, optimizer, epoch, epoches, r
             running_loss = 0.0
         
     train_loss += [t_loss / train_i]
-    #TODO: stop using average loss ~ instead: every 10 batches or all
 
     print('Train Epoch: %d\t Average loss: %.6f' %
                     (epoch + 1, t_loss / train_i))
 
+    torch.save(net.state_dict(), '../Training/training_epoch-{}'.format(save_epoch))
+
     print('*** Finished training in epoch {} ***'.format(epoch + 1))
 
 
-def test_net(testloader, classes, net, criterion, epoch, true_positives, true_negatives, false_positives, false_negatives, validation_loss, test_accuracy, f1_score, test_images, test_image_label, test_image_pred):
+def test_net(testloader, classes, b_size, net, criterion, epoch, true_positives, true_negatives, false_positives, false_negatives, validation_loss, test_accuracy, f1_score, test_images, test_image_label, test_image_pred):
     with torch.no_grad():
         val_loss = 0.0
         val_i = 0
@@ -91,11 +102,20 @@ def test_net(testloader, classes, net, criterion, epoch, true_positives, true_ne
                
         validation_loss += [val_loss / val_i]
 
-        precision = true_positives / (true_positives + false_positives)
-        recall = true_positives / (true_positives + false_negatives)
+        if (false_positives == 0 and true_positives == 0):
+            precision = 0
+        else:
+            precision = true_positives / (true_positives + false_positives)
+        if (false_negatives == 0 and true_positives == 0):
+            recall = 0
+        else:
+            recall = true_positives / (true_positives + false_negatives)
         numerator = precision * recall
         denominator = precision + recall
-        f1 = 2 * (numerator / denominator)
+        if (denominator == 0):
+            f1 = 0
+        else:
+            f1 = 2 * (numerator / denominator)
 
         print('Test Epoch: %d\t Average validation loss: %.6f\n Precision: {}\t Recall: {}'.format(precision, recall) %
                     (epoch + 1, val_loss / val_i))
@@ -107,7 +127,7 @@ def test_net(testloader, classes, net, criterion, epoch, true_positives, true_ne
 
 
 
-def loop_epoches(trainloader, testloader, classes, net, criterion, optimizer):
+def loop_epoches(trainloader, testloader, classes, b_size, net, criterion, optimizer, number_of_epoches):
     epoches = []
     train_loss = []
 
@@ -124,7 +144,7 @@ def loop_epoches(trainloader, testloader, classes, net, criterion, optimizer):
 
     save_epoch = 0
 
-    for epoch in range(40):
+    for epoch in range(number_of_epoches):
         running_loss = 0.0
         t_loss = 0.0
         train_i = 0
@@ -134,7 +154,8 @@ def loop_epoches(trainloader, testloader, classes, net, criterion, optimizer):
 
         print('*** Start training in epoch {} ***'.format(epoch + 1))
         net.train()
-        train_net(trainloader, classes, net, criterion, optimizer, epoch, epoches, running_loss, t_loss, train_i, train_loss, train_images, train_image_label, save_epoch)
+        train_net(trainloader, classes, b_size, net, criterion, optimizer, epoch, epoches, running_loss, t_loss, train_i, train_loss, train_images, train_image_label, save_epoch)
+        store_json(train_loss, epoches)
 
         print('*** Start testing in epoch {} ***'.format(epoch + 1))
         true_positives = 0.0
@@ -143,11 +164,11 @@ def loop_epoches(trainloader, testloader, classes, net, criterion, optimizer):
         false_negatives = 0.0     
 
         net.eval()
-        test_net(testloader, classes, net, criterion, epoch, true_positives, true_negatives, false_positives, false_negatives, validation_loss, test_accuracy, f1_score, test_images, test_image_label, test_image_pred)
+        test_net(testloader, classes, b_size, net, criterion, epoch, true_positives, true_negatives, false_positives, false_negatives, validation_loss, test_accuracy, f1_score, test_images, test_image_label, test_image_pred)
 
     return net, epoches, train_loss, train_images, train_image_label, test_accuracy, f1_score, validation_loss, test_images, test_image_label, test_image_pred
 
 
-def train_test(trainloader, testloader, classes, net, criterion, optimizer, date_time):
-    net_trained, epoches, train_loss, train_images, train_image_label, test_accuracy, f1_score, validation_loss, test_images, test_image_label, test_image_pred = loop_epoches(trainloader, testloader, classes, net, criterion, optimizer)
+def train_test(trainloader, testloader, classes, b_size, net, criterion, optimizer, number_of_epoches):
+    net_trained, epoches, train_loss, train_images, train_image_label, test_accuracy, f1_score, validation_loss, test_images, test_image_label, test_image_pred = loop_epoches(trainloader, testloader, classes, b_size, net, criterion, optimizer, number_of_epoches)
     return epoches, train_loss, test_accuracy, f1_score, validation_loss, train_images, train_image_label, test_images, test_image_label, test_image_pred
